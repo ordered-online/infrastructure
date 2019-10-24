66f028f5bf72cd9e095feb8458a317a1ba961097
Felix Kästner <kaestner.fe@gmail.com>
2019-10-24 13:00:35 +0200

    Initial Commit

---
+++ django/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+APP_DIR=$( dirname "$(find . -type f -name "requirements.txt")" )
+
+python -m pip install -r "${APP_DIR}/requirements.txt"
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "Postgres started"
+fi
+
+python "${APP_DIR}/manage.py" flush --no-input
+python "${APP_DIR}/manage.py" migrate
+
+exec "$@"
