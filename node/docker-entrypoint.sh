a20d699cbf313637677e3575e80c7a585bd8bb99
Felix Kästner <kaestner.fe@gmail.com>
2019-12-30 15:58:39 +0100

    Fix build commands for manager app

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 yarn --cwd /usr/src/app
-expo-cli build:web /usr/src/app/packages/manager
+yarn --cwd /usr/src/app build:api
+yarn --cwd /usr/src/app build:components
+expo-cli build:webr /usr/src/app/packages/manager
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
