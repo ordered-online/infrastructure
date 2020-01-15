61f4a79adaf8009178000ccbf6d810df6d22d7e9
PhilippMatthes <github@philippmatth.es>
2020-01-15 09:39:27 +0100

    Load fixtures on build if data directory exists

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -6,12 +6,17 @@ if ! [ -z "$SQL_HOST" ] && [ -z "$SQL_PORT" ]; then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-        sleep 0.1
+        sleep 1.0
     done
 
     echo "Postgres started"
 fi
 
-python "/usr/src/app/manage.py" migrate
+python /usr/src/app/manage.py migrate
+
+if [ -d "/usr/src/app/data/" ]; then
+    echo "Found data directory. Installing fixtures..."
+    python /usr/src/app/manage.py loaddata $(find /usr/src/app/data/ -name "*.json")
+fi
 
 exec "$@"
